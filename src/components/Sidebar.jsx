b4cd7b34a878152dc3bd0c7f80b9e1e25f414882
Auxtine <augustineezra@gmail.com>
2024-12-21 10:52:00 +0100

    few changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,8 +17,8 @@ const DesktopSidebar = () => {
     <div className="p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block">
       <div className="flex flex-col gap-20 sticky top-10 left-0">
         <div className="w-full">
-          <img src="public/hezlar-kitchen-lg.jpg" alt="logo" className="hidden md:block" />
-          <img src="public/hezlar-kitchen-mobile.jpg" alt="logo" className="block md:hidden" />
+          <img src="/hezlar-kit-lg.jpg" alt="logo" className="hidden md:block" />
+          <img src="/hezlar-kit-mobile.jpg" alt="logo" className="block md:hidden" />
         </div>
         <ul className="flex flex-col items-center md:items-start gap-8">
           <Link to={"/"} className="flex gap-1">
